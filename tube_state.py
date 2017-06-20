ae1c64dfaeb160df9db3aaa77a4c8fb64f4898bc
robmarkcole <robmarkcole@gmail.com>
2017-06-20 20:17:26 +0100

    Validate tube lines & get reasons for disruptions
    Validate entries in config against list of allowed tube lines.
    Get the reasons for any disruptions

--- tube_state.py
+++ tube_state.py
@@ -10,6 +10,7 @@ import voluptuous as vol
 import logging
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
+from homeassistant.const import ATTR_ATTRIBUTION
 
 from datetime import datetime, timedelta
 import requests
@@ -17,50 +18,57 @@ import json
 
 SCAN_INTERVAL = timedelta(minutes=1)
 
-DOMAIN = 'tube_state'     # the top level variable - should match the name of the compoenent
+DOMAIN = 'tube_state'     #  must match the name of the compoenent
 CONF_LINE= 'line'         # get from config, look for 'line' and assign value to CONF_LINE
+ATTRIBUTION = "Powered by TfL Open Data"
+TUBE_LINES= ['Bakerloo',
+             'Central',
+             'Circle',
+             'District',
+             'Hammersmith-city',
+             'Jubilee',
+             'Metropolitan',
+             'Northern',
+             'Piccadilly',
+             'Victoria',
+             'Waterloo-city']
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
-      vol.Required(CONF_LINE): cv.string,    # check that mandatory variables are provided
+      vol.Required(CONF_LINE): vol.In(TUBE_LINES)    # Check a valid line
     })
 }, extra=vol.ALLOW_EXTRA)
 
 _LOGGER = logging.getLogger(__name__)      # enable logging to console
 
 
-def setup_platform(hass, config, add_devices, discovery_info=None):     # this config is a dict of the user supplied variables
-    sensors = []          # config[DOMAIN].get(CONF_LINE, DEFAULT_LINE)      # create local varlaible, if CONF_LINE exists, use, else
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    sensors = []
     for tube in config.get(CONF_LINE):
         sensors.append(LondonTubeSensor(tube))
 
     add_devices(sensors)
-    _LOGGER.info("The 'tube_state' component is ready!")
+    _LOGGER.info("The tube_state component is ready!")
 
 
 class LondonTubeSensor(Entity):    # Entity
     """
-    Sensor that reads the status of a tube lines.
+    Sensor that reads the status of a tube lines using the TFL API.
     """
 
-    TRANSPORT_API_URL_BASE = "https://api.tfl.gov.uk/line/mode/tube/status"
+    API_URL_BASE = "https://api.tfl.gov.uk/line/{}/status"
     ICON = 'mdi:subway'
 
     def __init__(self, line):
         """Initialize the sensor."""
         self._data = {}
-        self._url = self.TRANSPORT_API_URL_BASE
+        self._url = self.API_URL_BASE
         self._line = line
-        self._state = None
-
-    @property
-    def line(self):
-        """Return the name of the sensor."""
-        return self._line
+        self._state = 'Updating'
 
     @property
     def name(self):
-        """Return the name of the sensor."""
+        """Return the line name of the sensor."""
         return self._line
 
     @property
@@ -74,7 +82,7 @@ class LondonTubeSensor(Entity):    # Entity
         return self.ICON
 
     @property
-    def unit_of_measurement(self):
+    def unit_of_measurement(self):    # Do I need?
         """Return the unit this state is expressed in."""
         return ""
 
@@ -82,17 +90,19 @@ class LondonTubeSensor(Entity):    # Entity
         """Perform an API request."""
 
         try:
-            response = requests.get(self._url)
+            response = requests.get(self._url.format(self._line.lower()))
             response.raise_for_status()
+            self._data = response.json()[0]['lineStatuses']   # convert to json and get statuses list
+            self._statuses = [status['statusSeverityDescription'] for status in self._data]
 
-            lines = [line['id'] for line in response.json()]
-            statuses = [line['lineStatuses'][0]['statusSeverityDescription'] for line in response.json()]
-            status_dict = {key:value for key, value in zip(lines, statuses)}
-
-            self._data = status_dict
-            self._state = status_dict[self.line.lower()]   # Get lower case as requireds
+            if 'Good Service' in self._statuses:   # if good status, this is the only status returned
+                self._state = 'Good Service'
+                self._description = 'Nothing to report'
+            else:
+                self._state = 'Disruptions'
+                self._description = [status['reason'] for status in self._data] # get the reasons
 
         except requests.RequestException as req_exc:
             print(
-                'Invalid response from transportapi.com: %s', req_exc
+                'Invalid response from API: %s', req_exc
             )
