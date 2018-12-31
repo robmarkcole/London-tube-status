5c4c927b821c0fb2aa16f303c472ed26fa257cb3
robmarkcole <robmarkcole@gmail.com>
2018-12-31 09:43:48 +0000

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1'
+
+REQUIRES = [
+    'requests',
+]
+
+setup(
+    name='london-tube-status',
+    version=VERSION,
+    url='https://github.com/robmarkcole/London-tube-status',
+    author='Robin Cole',
+    author_email='robmarkcole@gmail.com',
+    description='Parse London tube data into a dictionary',
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    license='Apache License, Version 2.0',
+    python_requires=">=3.5",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"]
+)
